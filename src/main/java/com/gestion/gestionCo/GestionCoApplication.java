325c4c381101a601e080c05cce57114bf0455641
Mohamed Yacine <my.houbi@gmail.com>
2021-02-08 18:34:39 +0100

    Commit initial

---
+++ src/main/java/com/gestion/gestionCo/GestionCoApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.gestionCo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/gestionCo/GestionCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.gestionCo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
